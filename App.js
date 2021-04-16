bdca89f01f7f9a7f9fcbbab3867776de41d448c6
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-04-15 22:13:44 -0300

    add auth screen, loinform

--- App.js
+++ App.js
@@ -1,13 +1,45 @@
-import React from 'react';
-
-import { SafeAreaView,Text,View } from 'react-native'
+import React, { Fragment, useEffect, useState } from 'react';
+import firebase from './src/utils/firebase';
+import Auth from './src/components/Auth';
+import 'firebase/auth';
+import { SafeAreaView,Text,View,StyleSheet, StatusBar } from 'react-native'
 
 const App = () => {
+  const [user, setUser] = useState(undefined);
+  useEffect(()=>{
+    firebase.auth().onAuthStateChanged((response)=>{
+      // console.log('usuario:log',response);
+      setUser(response);
+    });
+  },[]);
+  if(user === undefined){
+    return null;
+  }
+
   return ( 
-    <SafeAreaView>
-      <Text>Esteban</Text>
+    <Fragment>
+    {/* <StatusBar barStyle="light-content" translucent/> */}
+    <StatusBar  backgroundColor="transparent" translucent barStyle="light-content"/>
+
+    <SafeAreaView
+    style={styles.background}
+    >
+      {user ?(
+        <Text>Estas logueado</Text>
+      ):(
+        <Text>No estas logueado</Text>
+      )
+    }
     </SafeAreaView>
+    </Fragment>
    );
 }
  
 export default App;
+
+const styles=StyleSheet.create({
+    background:{
+      backgroundColor:'#15212b',
+      height:'100%'
+    }
+})
