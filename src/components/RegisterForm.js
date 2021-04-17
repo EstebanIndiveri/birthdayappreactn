b26ff734b9f64b009d6cd522ec83f8ed658ef68d
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-04-16 22:04:17 -0300

    complete registerform

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,17 +1,46 @@
 import React from 'react'
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity,TextInput,View } from 'react-native'
 
 export default function RegisterForm(props) {
     const{changeForm}=props
+    const register=()=>{
+        console.log('registrand')
+    }
     return (
-        <View>
-            <Text style={{color:'white'}}>RegisterForm</Text>
-              <TouchableOpacity
+        <>
+            <TextInput
+            placeholder="Correo electrónico"
+            placeholderTextColor='#969696'
+            style={styles.input}
+            />
+            <TextInput
+            placeholder="Contraseña"
+            secureTextEntry={true}
+            placeholderTextColor='#969696'
+            style={styles.input}
+            />
+            <TextInput
+            placeholder="Repetir contraseña"
+            placeholderTextColor='#969696'
+            secureTextEntry={true}
+
+            style={styles.input}
+            />
+            <TouchableOpacity
+            onPress={register}
+            >
+            <Text style={styles.btnText}>Registrate</Text>
+            </TouchableOpacity>
+
+            <View style={styles.login}>
+            <TouchableOpacity
             onPress={changeForm}
+            
             >
             <Text style={styles.btnText}>Iniciar Sesión</Text>
             </TouchableOpacity>
-        </View>
+            </View>
+        </>
     )
 }
 
@@ -19,5 +48,22 @@ const styles = StyleSheet.create({
     btnText:{
         color:'#FFF',
         fontSize:18,
+    },
+    input:{
+        height:50,
+        color:'#FFF',
+        width:'80%',
+        marginBottom:25,
+        backgroundColor:"#1e3040",
+        paddingHorizontal:20,
+        borderRadius:50,
+        fontSize:18,
+        borderWidth:1,
+        borderColor:'#1e3040'
+    },
+    login:{
+        flex:1,
+        justifyContent:'flex-end',
+        marginBottom:15
     }
 })
