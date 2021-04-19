54a71606b2067c060378c244106ca1900794744e
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-04-18 21:53:03 -0300

    add datepicker

--- src/components/AddBirthday.js
+++ src/components/AddBirthday.js
@@ -1,17 +1,44 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,TextInput } from 'react-native'
 
 export default function AddBirthday() {
     return (
-        <View>
-            <Text>AddBirthDay</Text>
-            <Text>AddBirthDay</Text>
-            <Text>AddBirthDay</Text>
-            <Text>AddBirthDay</Text>
-            <Text>AddBirthDay</Text>
+        <>
+           <View style={styles.container}>
+               <TextInput
+               placeholder="Nombre"
+               placeholderTextColor="#969696"
+                style={styles.input}
+               />
+               <TextInput
+               placeholder="Apellido"
+               placeholderTextColor="#969696"
+                style={styles.input}
+               />
+               
+           </View>
 
-        </View>
+        </>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        height:'100%',
+        width:'100%',
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    input:{
+        height:50,
+        color:'#FFF',
+        width:'80%',
+        marginBottom:25,
+        backgroundColor:'#1e3040',
+        paddingHorizontal:20,
+        borderRadius:50,
+        fontSize:18,
+        borderWidth:1,
+        borderColor:'#1e3040'
+    }
+})
